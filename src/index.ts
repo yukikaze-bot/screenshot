c8cdf3dcc6d7ed7fbbd9cbbee02379c2142346e1
1chiSensei <mail@tomio.codes>
2021-07-11 15:52:55 +0800

    feat: add redis caching

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,38 @@ import { authorization } from './middlewares/authorization';
 import adblocker from 'puppeteer-extra-plugin-adblocker';
 import stealth from 'puppeteer-extra-plugin-stealth';
 import puppeteer from 'puppeteer-extra';
+import compression from 'compression';
 import express from 'express';
 import helmet from 'helmet';
+import Redis from 'ioredis';
 
 const app = express();
+const redis = new Redis(process.env.REDIS_URL!);
 
 puppeteer.use(adblocker());
 puppeteer.use(stealth());
 
 app.use(express.json());
 app.use(helmet());
+app.use(compression({ level: 9 }));
 app.use(authorization);
 app.set('json spaces', 4);
 
 app.get('/', async (req, res) => {
+	const browser = await puppeteer.launch({
+		executablePath: '/usr/bin/chromium-browser',
+		args: ['--no-sandbox', '--disable-dev-shm-usage', '--shm-size=3gb']
+	});
+
 	try {
 		const { url } = req.query;
 		const width = req.query.width ? Number(req.query.width) : 1920;
 		const height = req.query.height ? Number(req.query.height) : 1080;
 		const fullPage = req.query.full === 'yes';
-		const browser = await puppeteer.launch({
-			executablePath: '/usr/bin/chromium-browser',
-			args: ['--no-sandbox', '--disable-dev-shm-usage', '--shm-size=3gb']
-		});
+		const inCache = await redis.getBuffer(url as string).catch(() => null);
+
+		if (inCache) return res.type('png').status(200).send(inCache);
+
 		const page = await browser.newPage();
 
 		page.setDefaultNavigationTimeout(0);
@@ -36,10 +45,12 @@ app.get('/', async (req, res) => {
 
 		await page.close();
 		await browser.close();
+		await redis.setex(url as string, 604800, ss);
 
-		res.type('png').status(200).send(ss);
+		return res.type('png').status(200).send(ss);
 	} catch (e) {
-		res.status(500).send(e);
+		await browser.close();
+		return res.status(500).send(e);
 	}
 });
 
