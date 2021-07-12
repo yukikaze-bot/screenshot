02e21802ba69be030a5543d9d2b668d1d1fbf8dd
1chiSensei <mail@tomio.codes>
2021-07-12 11:07:22 +0800

    feat: send 401 on unauthorized

--- src/middlewares/authorization.ts
+++ src/middlewares/authorization.ts
@@ -2,7 +2,8 @@ import type { Request, Response, NextFunction } from 'express';
 
 export const authorization = (req: Request, res: Response, next: NextFunction) => {
 	if (!process.env.AUTH) return next();
-	if (process.env.AUTH !== req.header('Authorization')) return res.sendStatus(400);
+	if (!req.header('Authorization')) return res.sendStatus(400);
+	if (process.env.AUTH !== req.header('Authorization')) return res.sendStatus(401);
 
 	return next();
 };
